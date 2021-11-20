e65f1e7f45c13560a2fc02ebea6d46ad9ffb7e3b
jackguoxz <jackguoxz@hotmail.com>
2021-11-20 15:50:12 +0800

    remove files
    Commit

--- Itinerary-Rest/src/main/java/com/okta/developer/docker_microservices/ui/controller/ItineraryController.java
+++ Itinerary-Rest/src/main/java/com/okta/developer/docker_microservices/ui/controller/ItineraryController.java
@@ -1,7 +1,5 @@
 package com.okta.developer.docker_microservices.ui.controller;
 
-import com.okta.developer.docker_microservices.ui.dto.ItineraryDto;
-import com.okta.developer.docker_microservices.ui.dto.TeachingClassDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -12,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/")
@@ -33,15 +29,6 @@ public class ItineraryController {
         return new ModelAndView("index");
     }
 
-    /*
-    @GetMapping("/classes")
-    public ResponseEntity<List<TeachingClassDto>> listClasses(){
-
-        return restTemplate
-                .exchange("http://"+ serviceHost +"/class", HttpMethod.GET, null,
-                        new ParameterizedTypeReference<List<TeachingClassDto>>() {});
-    }
-    */
 
     @GetMapping("/classes")
     public ResponseEntity<List<String>> listClasses(){
