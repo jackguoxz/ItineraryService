529577be2355cf4431bfdfe74c26759db1877772
jackguoxz <jackguoxz@hotmail.com>
2021-11-26 23:16:14 +0800

    Update function to use Dijkstra

--- Itinerary-service/src/main/java/com/example/developer/docker_microservices/service/controllers/ItineraryController.java
+++ Itinerary-service/src/main/java/com/example/developer/docker_microservices/service/controllers/ItineraryController.java
@@ -36,7 +36,7 @@ public class ItineraryController {
             return  result;
         };
         int cityId=Integer.parseInt(id);
-        List<String> shortestItineraryByConnection = itineraryService.getShortestItineraryByConnection(cityId);
+        List<String> shortestItineraryByConnection = itineraryService.getShortestItineraryByConnectionByDijkstra(cityId);
         for (int i = 0; i < shortestItineraryByConnection.size(); i++) {
             result.add(shortestItineraryByConnection.get(i));
         }
@@ -53,54 +53,12 @@ public class ItineraryController {
             return  result;
         };
         int cityId=Integer.parseInt(id);
-        List<String> shortestItineraryByTime = itineraryService.getShortestItineraryByTime(cityId);
+        List<String> shortestItineraryByTime = itineraryService.getShortestItineraryByTimeByDijkstra(cityId);
         for (int i = 0; i < shortestItineraryByTime.size(); i++) {
             result.add(shortestItineraryByTime.get(i));
         }
         return result;
     }
-
-    @RequestMapping(value = "/listitinerary/{id}", method = RequestMethod.GET, produces = {"application/json;charset=UTF-8"})
-    @ResponseBody
-    public List<String> getShortestItinerary(@PathVariable String id){
-        List<String> result = new ArrayList<>();
-        if(checkCityId(id))
-        {
-            result.add("Invalid Original City Id");
-            return  result;
-        };
-        int cityId=Integer.parseInt(id);
-        List<String> shortestItineraryByConnection = itineraryService.getShortestItineraryByConnection(cityId);
-        List<String> shortestItineraryByTime = itineraryService.getShortestItineraryByTime(cityId);
-        result.add("shortestItineraryByConnection");
-        for (int i = 0; i < shortestItineraryByConnection.size(); i++) {
-            result.add(shortestItineraryByConnection.get(i));
-        }
-        result.add("shortestItineraryByTime");
-        for (int i = 0; i < shortestItineraryByTime.size(); i++) {
-            result.add(shortestItineraryByTime.get(i));
-        }
-        return result;
-    }
-
-    @RequestMapping(value = "/listitinerary/bymap/{id}", method = RequestMethod.GET, produces = {"application/json;charset=UTF-8"})
-    @ResponseBody
-    public Map<String,List<String>> getShortestItineraryByMap(@PathVariable String id){
-        Map<String,List<String>> map =new HashMap();
-        if(checkCityId(id)) {
-            List<String> string=new ArrayList<>();
-            string.add(id);
-            map.put("Invalid Original City Id",string);
-            return  map;
-        };
-        int cityId=Integer.parseInt(id);
-        List<String> shortestItineraryByConnection = itineraryService.getShortestItineraryByConnection(cityId);
-        List<String> shortestItineraryByTime = itineraryService.getShortestItineraryByTime(cityId);
-        map.put("connection",shortestItineraryByConnection);
-        map.put("time",shortestItineraryByTime);
-        return map;
-    }
-
 
     @RequestMapping(value = "/listitinerary/dijkstra/{id}", method = RequestMethod.GET, produces = {"application/json;charset=UTF-8"})
     @ResponseBody
